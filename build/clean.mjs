3b0efbcfa45189c8fc79b7e63cdc1f9eeee34229
Nolan Hawkins <nolanhhawkins@gmail.com>
2025-03-23 09:11:03 -0400

    initial cleaning scripts

--- build/clean.mjs
+++ build/clean.mjs
@@ -1,23 +1,75 @@
+import { stringify } from "yaml";
 import fs from "fs";
-import Showdown from "showdown";
 
-const POST_DIR = "./posts";
+import { normalizePosts, parsePosts } from "./parse.mjs";
+import path from "path";
+/** @import { PostInfo } from "./parse.mjs" */
 
-function parsePost(filePath) {}
-function parsePosts() {
-  const dir = fs.readdirSync(POST_DIR);
-  const posts = {
-    authors: {},
-    books: {},
-    locations: {},
-    tags: {},
-    languages: {},
-  };
+const SAVED_FIELDS = {
+  default: ["layout", "id", "name"],
+  book: [
+    "subtitle",
+    "author",
+    "authorId",
+    "pageCount",
+    "written",
+    "stars",
+    "tags",
+    "mapping",
+    "gender",
+    "translatedFrom",
+    "series",
+    "previouslyRead",
+  ],
+  author: ["gender", "mapping"],
+  tags: ["specialization"],
+  location: ["latitude", "longitude"],
+};
 
-  for (const ent of dir) {
-    parsePost(ent);
+/**
+ *
+ * @param {PostInfo} post
+ */
+function postAsFile(post, allPosts) {
+  const filename =
+    post.layout === "book"
+      ? `posts/${post.date}-${post.id}.md`
+      : `meta/${post.layout}/${post.id}.md`;
+  const savedFields = [
+    ...SAVED_FIELDS.default,
+    ...(SAVED_FIELDS[post.layout] ?? []),
+  ];
+  const headerData = Object.fromEntries(
+    savedFields.flatMap((field) => (post[field] ? [[field, post[field]]] : []))
+  );
+  if (headerData.tags) {
+    headerData.tags = headerData.tags.join(" ");
   }
-  console.log(Showdown);
+  if (headerData.mapping) {
+    headerData.mapping = allPosts.find(
+      ({ layout, id }) => layout === "location" && id === headerData.mapping[0]
+    )?.name;
+  }
+
+  const content = `---\n${stringify(headerData)}---\n${
+    post.content?.length ? `${post.content}` : ""
+  }`;
+  return [filename, content];
 }
 
-parsePosts();
+async function rewriteFiles() {
+  const allPosts = await normalizePosts(parsePosts());
+  const postFiles = allPosts.map((post, _, allPosts) =>
+    postAsFile(post, allPosts)
+  );
+
+  for (const [filename, content] of postFiles) {
+    if (filename) {
+      const fullPath = `./posts2/${filename}`;
+      fs.mkdirSync(path.dirname(fullPath), { recursive: true });
+      fs.writeFileSync(fullPath, content);
+    }
+  }
+}
+
+rewriteFiles();
--- build/parse.mjs
+++ build/parse.mjs
@@ -1,49 +1,73 @@
 import fs from "fs";
-import { parse } from "yaml";
-import { Converter } from "showdown";
+import path from "path";
 
-const POST_DIR = "./posts";
-const CONVERTER = new Converter();
+import { parse } from "yaml";
+import showdown from "showdown";
+
+import { locate } from "./locate.mjs";
+
+const CONTENT_DIR = "./content";
+
+const CONVERTER = new showdown.Converter();
 
 /**
- * @typedef {{
+ * @typedef {{layout: string; id: string; name?: string; tags?: string[]; content?: string; contentHtml?: string}} GenericPost
+ * @typedef {GenericPost & {layout: "tag"; specialization?: "language" | "series" }} TagInfo
+ * @typedef {GenericPost & {layout: "location"; latitude: number; longitude: number;}} LocationInfo
+ * @typedef {GenericPost & {layout: "author";  gender?: "m" | "f"; mapping?: string[];}} AuthorInfo
+
+* @typedef {Omit<AuthorInfo, "layout"> & {
  *    layout: "book";
- *    id: string;
- *    title: string; 
- *    subtitle?: string; 
+ *    subtitle?: string;
  *    author: string;
- *    date?: Date;
+ *    authorId: string;
+ *    date: string;
+ *    previouslyRead?: string[];
  *    pageCount?: number;
  *    written?: number;
  *    stars?: number;
- *    tags?: string[];
  *    mapping?: string[];
  *    gender?: "m" | "f";
  *    translatedFrom?: string;
- *  }} BookInfo
+ *    series?: string;
+ *    content?: string;
+ *    contentHtml?: string;
+ * }} BookInfo
  * @typedef {{
  *    layout: "book" | "post";
  *    title?: string;
  *    subtitle?: string;
- *    author?: string; 
- *    date?: string; 
- *    pageCount?: number; 
- *    written?: number; 
- *    stars?: number; 
- *    tags?: string; 
- *    mapping?: string; 
+ *    author?: string;
+ *    date: string;
+ *    pageCount?: number;
+ *    written?: number;
+ *    stars?: number;
+ *    tags?: string;
+ *    mapping?: string;
  *    gender?: "m" | "f";
  *    translatedFrom?: string;
+ *    series?: string;
  * }} RawBookInfo
- * 
- * @typedef {{layout: "location"; id: string; latitude: number; longitude: number;}} LocationInfo
- * @typedef {{layout: "author"; id: string; name: string; gender?: "m" | "f"; mapping?: string;}} AuthorInfo
- * @typedef {{layout: "tag"; id: string; }} TagInfo
+ *
  * @typedef {BookInfo | LocationInfo | AuthorInfo | TagInfo} PostInfo
  */
 
 /**
- * @param {string} mapping Flat mapping string used for in-markdown-representation
+ * @param {string | null | undefined} str string to slugify
+ * @returns {string} slug like `A-Clockwork-Orange` for URLs and ids
+ */
+function slugify(str) {
+  if (!str) {
+    return "-";
+  }
+  return str
+    .replaceAll(" ", "-")
+    .replaceAll(/[^a-zA-Z0-9\-]/g, "")
+    .replaceAll("--", "-");
+}
+
+/**
+ * @param {string} mapping Flat mapping string used for in-markdown-representation, e.g. New York, USA, North America
  * @returns {string[]} Mapping location unpacked for regular data analysis
  */
 function unpackMap(mapping) {
@@ -51,7 +75,7 @@ function unpackMap(mapping) {
   if (mapParts) {
     return [...mapParts].map((_, i) => mapParts.slice(i).join(", "));
   }
-  return []
+  return [];
 }
 /**
  * @param {string[]} mapping Mapping location unpacked for regular data analysis
@@ -61,16 +85,15 @@ function packMap(mapping) {
   return mapping?.[0]?.split(", ");
 }
 
-/** @typedef {{file: {header: Object, content: string}, data?: PostInfo}} Post*/
 /**
- * 
- * @param {*} dir 
- * @param {*} file 
- * @returns {Post}
+ *
+ * @param {*} dir
+ * @param {*} filename
+ * @returns {PostInfo}
  */
-function parsePost(dir, file) {
+function parsePost(dir, filename) {
   let content = "";
-  const fileContent = fs.readFileSync(`${dir}/${file}`, "utf-8");
+  const fileContent = fs.readFileSync(`${dir}/${filename}`, "utf-8");
   let yamlContent = "";
   let state = 0;
   for (const line of fileContent.split("\n")) {
@@ -79,43 +102,186 @@ function parsePost(dir, file) {
       continue;
     }
     if (state === 1) {
-      yamlContent += `${line}\n`;
+      yamlContent += line + "\n";
     } else {
-      content += line;
+      content += line + "\n";
     }
   }
   /** @type Object */
   const header = parse(yamlContent);
   /** @type {PostInfo} */
   const data = { ...header };
-  data.review = content ? CONVERTER.makeHtml(content) : "";
-  data.tags = header.tags?.split(" ") ?? [];
-  data.file = file;
+  data.content = content?.trim() ?? undefined;
+  data.contentHtml = content ? CONVERTER.makeHtml(content) : undefined;
+  data.filename = filename;
+
+  if (header.tags) {
+    data.tags = header.tags?.split(" ");
+  }
+  if (header.mapping) {
+    data.mapping = unpackMap(header.mapping);
+  }
+
   if (header.layout === "post" || header.layout === "book") {
-    const [year, month, day, ...title] = filename.split("-");
-    data.title = " ".join(title).slice(0, -3);
-    data.readOn = `${year}-${month}-${day}`;
-    if(book.mapping) {
-      data.mapping = unpackMap(book.mapping);
+    const [year, month, day, ...title] = path
+      .basename(filename, ".md")
+      .split("-");
+    if (!data.name) {
+      data.name = title.join(" ").trim();
+      if (header.subtitle && data.name.endsWith(header.subtitle)) {
+        data.name = data.name.slice(0, -header.subtitle.length).trim();
+      }
     }
-    const mapParts = book.mapping?.split(", ");
-    if (mapParts) {
-      data.mapping = [...mapParts].map((_, i) => mapParts.slice(i).join(", "));
+
+    data.date = `${year}-${month}-${day}`;
+    if (header.layout === "post") {
+      data.layout = "book";
     }
   }
-  return { file: { header, content }, data };
+  return data;
 }
+/**
+ * Reads and parses posts from the content directory
+ * @returns {PostInfo[]} po
+ */
 export function parsePosts() {
-  const dir = fs.readdirSync(POST_DIR);
-  const posts = {
-    authors: {},
-    books: {},
-    locations: {},
-    tags: {},
-    languages: {},
+  return fs
+    .readdirSync(CONTENT_DIR, { recursive: true })
+    .flatMap((filename) =>
+      filename.endsWith(".md") ? [parsePost(CONTENT_DIR, filename)] : []
+    );
+}
+
+/**
+ *
+ * @param {PostInfo} post
+ */
+function assignIds(data) {
+  data.id = data.id ?? slugify(data.name);
+  data.layout = data.layout === "post" ? "book" : data.layout ?? "book";
+  return data;
+}
+
+/**
+ * @param {string} tag
+ * @param {"language" | "series" | undefined} specialization
+ * @returns {TagInfo} */
+function createTag(tag, specialization) {
+  const id = slugify(tag);
+  if (id !== tag) {
+    return { layout: "tag", id, name: tag, specialization };
+  }
+  return { layout: "tag", id: tag, specialization };
+}
+
+/**
+ * @param {string} name
+ * @param {"m" | "f" | undefined} gender
+ * @param {string[] | undefined} mapping
+ * @returns {AuthorInfo}
+ */
+function createAuthor(name, authorId, gender, mapping) {
+  const id = authorId ?? slugify(name);
+  return { layout: "author", id, name, gender, mapping };
+}
+
+function createLocation(name) {
+  const id = slugify(name);
+  return { layout: "location", id, name };
+}
+
+async function findLocation(location) {
+  if (location.latitude == null || location.latitude == null) {
+    const found = await locate(location.name);
+    if (found) {
+      location.latitude = found.latitude;
+      location.longitude = found.longitude;
+    }
+  }
+}
+
+/**
+ *
+ * @param {PostInfo[]} posts
+ * @returns {PostInfo[]} normalized post information
+ */
+export async function normalizePosts(rawPosts) {
+  const posts = rawPosts.map(assignIds);
+
+  /** @type {Record<PostInfo["layout"], Record<string, PostInfo>} */
+  const identifiedPosts = {
+    author: {},
+    book: {},
+    location: {},
+    tag: {},
   };
 
-  for (const ent of dir) {
-    const  = parsePost(POST_DIR, ent);
+  /** @param {PostInfo} post */
+  const identifyPost = (post) => {
+    const previous = identifiedPosts[post.layout][post.id];
+    if (post.layout === "book" && previous) {
+      console.warn("Overwriting book!", post.id, post.name);
+      identifiedPosts[post.layout][post.id] = {
+        ...post,
+        previouslyRead: [previous.date, ...(previous.previouslyRead ?? [])],
+      };
+    } else {
+      identifiedPosts[post.layout][post.id] = {
+        ...previous,
+        ...Object.fromEntries(
+          Object.entries(post).filter(([_k, v]) => v !== undefined)
+        ),
+      };
+    }
+    return post.id;
+  };
+
+  for (const post of posts) {
+    if (post.layout == null || identifiedPosts[post.layout] == null) {
+      console.warn(`Invalid post`, post);
+      continue;
+    }
+    identifyPost(post);
+    const p = identifiedPosts[post.layout][post.id];
+    if (p.mapping) {
+      p.mapping = p.mapping.map((loc) => identifyPost(createLocation(loc)));
+    }
+    if (p.tags) {
+      p.tags = p.tags.map((tag) => identifyPost(createTag(tag)));
+    }
+
+    if (p?.layout === "book") {
+      if (p.author) {
+        p.authorId = identifyPost(
+          createAuthor(p.author, p.authorId, p.gender, p.mapping)
+        );
+      }
+      if (p?.series) {
+        p.series = identifyPost(createTag(p.series, "series"));
+      }
+      if (post?.translatedFrom) {
+        identifyPost(createTag(post.translatedFrom, "language"));
+      }
+    }
   }
+  for (const book of Object.values(identifiedPosts.book)) {
+    if (
+      book.mapping === undefined &&
+      identifiedPosts.author[book.authorId]?.mapping
+    ) {
+      book.mapping = [...identifiedPosts.author[book.authorId].mapping];
+    }
+    if (
+      book.gender === undefined &&
+      identifiedPosts.author[book.authorId]?.gender
+    ) {
+      book.gender = identifiedPosts.author[book.authorId]?.gender;
+    }
+  }
+  for (const location of Object.values(identifiedPosts.location)) {
+    await findLocation(location);
+  }
+  return Object.values(identifiedPosts).flatMap((postMap) =>
+    Object.values(postMap)
+  );
 }
